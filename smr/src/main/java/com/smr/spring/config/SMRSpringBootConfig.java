1a63dc60abad9080d5264e84e5f662382a2278f5
seemyreels@gmail.com <seemyreels@gmail.com>
2016-12-03 23:41:43 +0530

    added project

---
+++ smr/src/main/java/com/smr/spring/config/SMRSpringBootConfig.java
@@ -0,0 +1,10 @@
+package com.smr.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value="com.smr")
+public class SMRSpringBootConfig {
+
+}
